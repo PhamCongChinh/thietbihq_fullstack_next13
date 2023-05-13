47c410c529d29e7b2ab939aaeb1d106fbd35ad42
PhamCongChinh <chinhpham1609@gmail.com>
2023-05-14 04:03:31 +0700

    aaaa

--- app/api/(data)/products/route.ts
+++ app/api/(data)/products/route.ts
@@ -1,9 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
-export const config = {
-    api: {
-      bodyParser: false
-    }
-}
+
 const GET = async (request: NextRequest) => {
     console.log("asdasdasdasdlkjalksdjkl")
     return NextResponse.json({message: "poa"})
